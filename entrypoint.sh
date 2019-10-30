46ee5545f4a9945df01a8de7c361323e9d8d3447
Junya Kaneko <junya@mpsamurai.org>
2019-10-30 19:18:04 +0900

    modified to execute terraform

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh -l
 
-echo $(ls $GITHUB_WORKSPACE)
+#chdir $GITHUB_WORKSPACE/$1
+terraform init -input=false
+terraform plan -input=false
+terraform apply -auto-approve
+sleep 30
+terraform destroy -auto-approve
